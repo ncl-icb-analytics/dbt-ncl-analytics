3caef0e8ef2fef7bddeea537e100fd3029a91eee
EddieDavison92 <143042680+EddieDavison92@users.noreply.github.com>
2025-08-21 13:25:51 +0100

    feat: commiting dbt packages for snowflake native execution

--- start_dbt.ps1
+++ start_dbt.ps1
@@ -1,7 +1,11 @@
-# start_dbt.ps1
-# Load project-specific environment variables
-Get-Content ".env" | ForEach-Object {
-  if ($_ -match '^([^=]+)=(.*)$' -and -not $_.StartsWith('#')) {
-      [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2], 'Process')
-  }
+  # start_dbt.ps1
+  # Activate virtual environment first
+  & "venv\Scripts\Activate.ps1"
+
+  # Load project-specific environment variables
+  Get-Content ".env" | ForEach-Object {
+    if ($_ -match '^([^=]+)=(.*)$' -and -not $_.StartsWith('#')) {
+        [System.Environment]::SetEnvironmentVariable($matches[1],
+$matches[2], 'Process')
+    }
 }
