2b118b471bbd7499212ff8e0d6ec9af7f542516f
PeterSNHS <peter.shakeshaft1@nhs.net>
2025-09-17 14:01:30 +0100

    feat: added distinct provider and tfc counts

--- models/commissioning/reporting/person_level/fct_person_wl_current_count_total.sql
+++ models/commissioning/reporting/person_level/fct_person_wl_current_count_total.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 /*
-Count of all active waiting lists per patient.
+Count of all active waiting lists per patient, number of providers and number of tfcs that these are recorded against.
 
 Clinical Purpose:
 - Care coordination management across providers
@@ -11,7 +11,9 @@ Includes ALL persons (active, inactive, deceased) following intermediate layer p
 
 SELECT
     patient_id,
-    COUNT(*) AS wl_current_total_count
+    COUNT(*) AS wl_current_total_count,
+    COUNT(DISTINCT provider_code) AS wl_current_distinct_providers_count,
+    COUNT(DISTINCT tfc_code) AS wl_current_distinct_tfc_count
 FROM {{ ref('int_wl_current') }}
 GROUP BY patient_id
 
