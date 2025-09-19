cc8838dc294f021476f87afd28a18a5a41f5270b
PeterSNHS <peter.shakeshaft1@nhs.net>
2025-09-19 18:23:58 +0100

    feat: person level table for number of open waiting lists by TFC (pivoted)

---
+++ models/commissioning/reporting/person_level/fct_person_wl_current_count_tfc.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='table')
+}}
+
+
+/*
+Number of open pathways under TFCs per patient.
+Clinical Purpose:
+- Care coordination management across providers
+
+Includes ALL persons (active, inactive, deceased) following intermediate layer principles.
+
+*/
+
+WITH TFC_COUNTS AS (
+    SELECT
+    patient_id,
+    tfc_code,
+    open_pathways
+    FROM {{ ref('int_wl_current') }}
+)
+SELECT
+*
+FROM TFC_COUNTS wl
+PIVOT
+(
+    SUM(open_pathways) FOR tfc_code IN (
+        SELECT
+        "BK_SpecialtyCode"
+        from "Dictionary"."dbo"."Specialties"
+        WHERE
+        "IsTreatmentFunction" = TRUE
+        )
+        DEFAULT ON NULL (0)
+) AS pvt
