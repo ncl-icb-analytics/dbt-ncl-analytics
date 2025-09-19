7a799b17e20d0a23fa7e951a1f3d612ec13c8031
PeterSNHS <peter.shakeshaft1@nhs.net>
2025-09-19 16:15:57 +0100

    Added flag for patients that have open waiting lists under the same TFC at multiple providers.

--- models/commissioning/reporting/person_level/fct_person_wl_current_count_total.sql
+++ models/commissioning/reporting/person_level/fct_person_wl_current_count_total.sql
@@ -1,7 +1,8 @@
 {{ config(materialized='table') }}
 
 /*
-Count of all active waiting lists per patient, number of providers and number of tfcs that these are recorded against.
+Count of all active waiting lists per patient, number of waiting lists at unique providers, 
+number of waiting lists under unique TFCs, and a flag for whether the patient has an open waiting list for the same TFC under multiple providers.
 
 Clinical Purpose:
 - Care coordination management across providers
@@ -9,11 +10,29 @@ Clinical Purpose:
 Includes ALL persons (active, inactive, deceased) following intermediate layer principles.
 */
 
+WITH DUPLICATE_TFCS AS (SELECT
+                        patient_id,
+                        tfc_code,
+                        provider_code,
+                        ROW_NUMBER() OVER(PARTITION BY patient_id, tfc_code ORDER BY provider_code) AS TFC_MULTIPLE_PROVIDERS_ROW_NUMBER
+                        FROM {{ ref('int_wl_current') }}
+                        GROUP BY 
+                        patient_id,
+                        tfc_code,
+                        provider_code),
+DUPLICATE_TFCS_GROUPED AS (SELECT 
+                           patient_id, 
+                           MAX(tfc_multiple_providers_row_number) AS TFC_MULTIPLE_PROVIDERS_MAX_ROW_NUMBER
+                           FROM DUPLICATE_TFCS
+                           GROUP BY 
+                           patient_id
+                           )
 SELECT
-    patient_id,
+    wl.patient_id,
     COUNT(*) AS wl_current_total_count,
-    COUNT(DISTINCT provider_code) AS wl_current_distinct_providers_count,
-    COUNT(DISTINCT tfc_code) AS wl_current_distinct_tfc_count
-FROM {{ ref('int_wl_current') }}
-GROUP BY patient_id
-
+    COUNT(DISTINCT wl.provider_code) AS wl_current_distinct_providers_count,
+    COUNT(DISTINCT wl.tfc_code) AS wl_current_distinct_tfc_count,
+    CASE WHEN MAX(dtfc.tfc_multiple_providers_max_row_number) > 1 THEN TRUE ELSE FALSE END AS same_tfc_multiple_providers_flag,
+FROM {{ ref('int_wl_current') }} wl
+LEFT JOIN DUPLICATE_TFCS_GROUPED dtfc ON wl.patient_id = dtfc.patient_id
+GROUP BY wl.patient_id
