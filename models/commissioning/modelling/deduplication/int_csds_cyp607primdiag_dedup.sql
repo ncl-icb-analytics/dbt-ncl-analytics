8fb7de274524d995d18facb19209e3fabf2714fb
Thomas Shallcross <thomas.shallcross@nhs.net>
2025-09-30 10:14:50 +0100

    feat: add csds deduplication tables

---
+++ models/commissioning/modelling/deduplication/int_csds_cyp607primdiag_dedup.sql
@@ -0,0 +1,10 @@
+{{
+    config(materialized = 'table')
+}}
+
+{{
+    deduplicate_csds(
+        csds_table = 'dev__modelling.dbt_staging.stg_csds_cyp607primdiag',
+        partition_cols = ['primary_diagnosis_coded_clinical_entry', 'diagnosis_scheme_in_use_community_care', 'diagnosis_date']
+    )
+}}
