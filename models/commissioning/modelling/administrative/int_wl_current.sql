9f3effc3ed2dcbf3d4bdef120a02dd2aef898fbd
Emily Baldwin <emily.baldwin20@nhs.net>
2025-10-03 17:06:06 +0100

    Feat: wl and eRS  models in new structure

--- models/commissioning/modelling/administrative/int_wl_current.sql
+++ models/commissioning/modelling/administrative/int_wl_current.sql
@@ -14,29 +14,11 @@ Includes ALL persons (active, inactive, deceased) following intermediate layer p
 
 */
 
-WITH date_corrected AS (
-    SELECT
-        Pseudo_NHS_NUMBER AS sk_patient_id,
-        organisation_identifier_code_of_provider AS provider_code,
-        activity_treatment_function_code AS tfc_code,
-        organisation_identifier_code_of_commissioner AS commissioner_code,
-        der_LSOA2021 AS lsoa_2021,
-        referral_request_received_date,
-        referral_to_treatment_period_start_date,
-        -- Correct all dates to Sundays (Snowflake syntax)
-        CASE 
-            WHEN DAYOFWEEKISO(Week_Ending_Date) = 7 THEN Week_Ending_Date  -- Already Sunday
-            ELSE DATEADD('day', -DAYOFWEEK(Week_Ending_Date), Week_Ending_Date)  -- Move to previous Sunday
-        END AS snapshot_date,
-        1 AS open_pathways
-    FROM {{ ref('stg_wl_wl_openpathways_data') }}
-    WHERE Week_Ending_Date IS NOT NULL
-      AND Week_Ending_Date <= CURRENT_DATE
-),
-most_recent_week AS (
+WITH most_recent_week AS (
     SELECT MAX(snapshot_date) AS max_date
-    FROM date_corrected
+    FROM {{ ref('stg_wl_wl_openpathways_data') }}
 )
+
 SELECT 
     dc.sk_patient_id,
     dc.provider_code,
@@ -47,5 +29,5 @@ SELECT
     dc.referral_to_treatment_period_start_date,
     dc.snapshot_date,
     dc.open_pathways
-FROM date_corrected dc
+FROM {{ ref('stg_wl_wl_openpathways_data') }} dc
 INNER JOIN most_recent_week mrw ON dc.snapshot_date = mrw.max_date
---
+++ models/commissioning/staging/stg_wl_wl_openpathways_data.sql
@@ -0,0 +1,21 @@
+{{
+    config(materialized = 'view')
+}}
+
+SELECT
+    Pseudo_NHS_NUMBER AS sk_patient_id,
+    organisation_identifier_code_of_provider,
+    activity_treatment_function_code,
+    organisation_identifier_code_of_commissioner,
+    der_LSOA2021,
+    referral_request_received_date,
+    referral_to_treatment_period_start_date,
+    -- Correct all dates to Sundays (Snowflake syntax)
+    CASE 
+        WHEN DAYOFWEEKISO(Week_Ending_Date) = 7 THEN Week_Ending_Date  -- Already Sunday
+        ELSE DATEADD('day', -DAYOFWEEK(Week_Ending_Date), Week_Ending_Date)  -- Move to previous Sunday
+    END AS snapshot_date,
+    1 AS open_pathways
+FROM {{ ref('raw_wl_wl_openpathways_data') }}
+WHERE Week_Ending_Date IS NOT NULL
+    AND Week_Ending_Date <= CURRENT_DATE
