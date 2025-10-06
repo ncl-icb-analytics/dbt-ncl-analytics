01c50ad904ec4bf17c28df3e159f57fdec12cffc
Emily Baldwin <emily.baldwin20@nhs.net>
2025-10-06 18:38:24 +0100

    -adding existing metrics (wl, measurement, behavioural). -adding pcn for application

--- models/products_direct_care/CLTCS/cohort_data.sql
+++ models/products_direct_care/CLTCS/cohort_data.sql
@@ -12,21 +12,23 @@ Clinical Purpose:
 
 Testing:
 - Actual table have additional fields and pull from a wide array of model outputs
+- Replace null handling and time spines with dbt functions in yml
 
 */
 
 with inclusion_list as (
-    select patient_id, olids_id
+    select patient_id, olids_id, pcn_code, pcn_name
     from {{ ref('inclusion_cohort')}}
     where eligible = 1
 )
 
 select il.patient_id
-    -- trajectories for sparkline visualisation
+    , il.pcn_code
+    -- trajectories for sparkline visualisation [add other domains - GP, Community, MH, total?]
     , tr.ae_encounters_sl
     , tr.ip_encounters_sl
     , tr.op_encounters_sl
-    -- local disease registries and counts
+    -- local disease registries and counts [ only of primary care for now, add acute/community etc data later ]
     , pc.has_atrial_fibrillation
     , pc.has_asthma
     , pc.has_cancer
@@ -40,7 +42,7 @@ select il.patient_id
     , pc.has_epilepsy
     , pc.has_familial_hypercholesterolaemia
     , pc.has_gestational_diabetes
-    , pc.has_frailty
+    , pc.has_frailty -- replace with ef2?
     , pc.has_heart_failure
     , pc.has_hypertension
     , pc.has_learning_disability
@@ -55,7 +57,7 @@ select il.patient_id
     , pc.has_severe_mental_illness
     , pc.has_stroke_tia
     , pc.total_conditions
-    , pc.total_qof_conditions
+    , pc.total_qof_conditions -- replace cambridge multimorbidity score/ similar complexity metric?
     , pc.total_non_qof_conditions
     , pc.cardiovascular_conditions
     , pc.respiratory_conditions
@@ -64,8 +66,55 @@ select il.patient_id
     , pc.musculoskeletal_conditions
     , pc.neurology_conditions
     , pc.geriatric_conditions
+    -- Lifestyle and behavioural factors
+    , br.smoking_status
+    , br.smoking_risk_sort_key
+    , br.bmi_category
+    , br.bmi_value
+    , br.bmi_risk_sort_key
+    , br.alcohol_status
+    , br.alcohol_risk_sort_key
+    -- current status to consider 
+    , ps.is_currently_pregnant 
+    -- dim_person_is_carer?
+    -- measurement flags (fully summaries elsewhere or held as array?)
+    , case when bp.latest_bp_date between dateadd(month, -6, current_date()) and current_date() then bp.is_overall_bp_controlled else null end as is_overall_bp_controlled -- assuming bp control only relevant if recent, replace with more nuanced logic that ascerts likely control given redings history and time
+    ,bp.is_overall_bp_controlled as is_most_recent_overall_bp_controlled
+    ,bp.latest_systolic_value
+    ,bp.latest_diastolic_value
+    ,bp.latest_bp_date
+    ,dcp.care_processes_completed
+    , case when dcp.hba1c_completed_in_last_12m = true then dcp.latest_hba1c_value else null end as latest_hba1c_value
+    ,dcp.latest_hba1c_date
+    -- Annual activity (OP, APC, UEC, MH, ASC, Community, GP appts, 111?)
+
+    -- Current waiting list counts and flags
+    ,zeroifnull(wl.wl_current_total_count) as wl_total_count
+    ,zeroifnull(wl.wl_current_distinct_providers_count) as wl_provider_count
+    ,zeroifnull(wl.wl_current_distinct_tfc_count) as wl_specialty_count
+    ,wl.same_tfc_multiple_providers_flag as has_same_tfc_multiple_providers_flag
+
+    -- polypharmacy, high risk drugs, suspected non-adherence
+
+    -- Current referrals
+
+    -- Current risk scores?
+
+    -- Other relevant annual activity (LTC LCS, C-LTCS review)
+
 from inclusion_list il
 left join {{ ref('trajectories') }} tr
     on il.patient_id = tr.patient_id
 left join {{ ref('dim_person_conditions')}} pc
     on il.olids_id = pc.person_id
+left join {{ref('fct_person_behavioural_risk_factors')}} br
+    on il.olids_id = br.person_id
+left join {{ref('fct_person_pregnancy_status')}} ps
+    on il.olids_id = ps.person_id
+left join {{ref('fct_person_bp_control')}} bp
+    on il.olids_id = bp.person_id 
+left join {{ref('fct_person_diabetes_8_care_processes')}} dcp
+    on il.olids_id = dcp.person_id
+left join {{ref('fct_person_wl_current_count_total')}} wl
+    on il.patient_id = wl.sk_patient_id
+
