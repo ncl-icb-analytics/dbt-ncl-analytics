f72cd9777b0728913a621de3a69371bb5ab889f5
Emily Baldwin <emily.baldwin20@nhs.net>
2025-10-09 17:37:18 +0100

    fis: missed sk_id

--- models/commissioning/staging/stg_sus_op_appointment.sql
+++ models/commissioning/staging/stg_sus_op_appointment.sql
@@ -5,7 +5,7 @@
 select  primarykey_id,
     appointment_identifier, 
     appointment_date,
-    appointment_patient_identity_nhs_number_value_pseudo, --as sk_patient_id,
+    appointment_patient_identity_nhs_number_value_pseudo as sk_patient_id,
     appointment_commissioning_service_agreement_provider, 
     appointment_care_location_site_code_of_treatment,
 from {{ ref('raw_sus_op_appointment') }}
