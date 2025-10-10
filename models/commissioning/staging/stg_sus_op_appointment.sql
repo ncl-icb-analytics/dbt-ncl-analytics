37d2abc0e67d0c667ac7dd46f24c6ec611ee560e
Emily Baldwin <emily.baldwin20@nhs.net>
2025-10-10 09:49:37 +0100

    Fix: missing op fields

--- models/commissioning/staging/stg_sus_op_appointment.sql
+++ models/commissioning/staging/stg_sus_op_appointment.sql
@@ -8,4 +8,13 @@ select  primarykey_id,
     appointment_patient_identity_nhs_number_value_pseudo as sk_patient_id,
     appointment_commissioning_service_agreement_provider, 
     appointment_care_location_site_code_of_treatment,
+    appointment_expected_duration,
+    appointment_outcome,
+    appointment_attended_or_dna,
+    appointment_first_attendance,
+    appointment_care_professional_main_specialty,
+    appointment_referral_priority_type,
+    appointment_care_professional_treatment_function,
+    appointment_commissioning_grouping_core_hrg,
+    appointment_commissioning_tariff_calculation_final_price
 from {{ ref('raw_sus_op_appointment') }}
