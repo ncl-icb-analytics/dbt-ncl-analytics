363357c07132fc8172cb561f0005f3ab853827c9
EddieDavison92 <143042680+EddieDavison92@users.noreply.github.com>
2025-10-10 09:44:30 +0100

    fix: add QUALIFY deduplication to stg_sus_apc_spell
    Deduplicate 19 duplicate primarykey_id records by keeping the most recent
    based on system_transaction_cds_activity_date and rownumber_id

--- models/commissioning/staging/stg_sus_apc_spell.sql
+++ models/commissioning/staging/stg_sus_apc_spell.sql
@@ -2,16 +2,20 @@
     config(materialized = 'view')
 }}
 
-select primarykey_id 
+select primarykey_id
     , spell_patient_identity_nhs_number_value_pseudo as sk_patient_id
     , spell_care_location_site_code_of_treatment
-    , spell_admission_date 
-    , spell_discharge_length_of_hospital_stay 
+    , spell_admission_date
+    , spell_discharge_length_of_hospital_stay
     , SPELL_COMMISSIONING_SERVICE_AGREEMENT_PROVIDER
-    , spell_commissioning_grouping_core_hrg 
-    , spell_clinical_coding_grouper_derived_primary_diagnosis  
-    , spell_clinical_coding_grouper_derived_secondary_diagnosis 
+    , spell_commissioning_grouping_core_hrg
+    , spell_clinical_coding_grouper_derived_primary_diagnosis
+    , spell_clinical_coding_grouper_derived_secondary_diagnosis
     , spell_admission_admission_sub_type
     , spell_commissioning_tariff_calculation_final_price
 
 from {{ ref('raw_sus_apc_spell') }}
+qualify row_number() over (
+    partition by primarykey_id
+    order by system_transaction_cds_activity_date desc, rownumber_id desc
+) = 1
