51e54bba5a5095529ec85731037374b581d4af2f
Thomas Shallcross <thomas.shallcross@nhs.net>
2025-10-13 17:56:38 +0100

    fix: refactord csds_deduplication macro

--- macros/deduplicate_csds.sql
+++ macros/deduplicate_csds.sql
@@ -1,21 +1,13 @@
-
+-- 2025-10-13: dear future Tom, I removed an inner join that is necessary for tables which don't have unique_service_request_identifier
+-- the inner join messed up the cyp201 deduplication process, so if you add it back in make sure it works ok
 {% macro deduplicate_csds(
         csds_table,
         partition_cols = []
         ) %}
 
-    {# detect whether csds_table is the cyp101referral table #}
-    {% set is_referral = 'cyp101referral' in csds_table|lower %}
-
-    {% if is_referral %}
-        {% set unique_id_col = 'tbl.unique_service_request_identifier' %}
-    {% else %}
-        {% set unique_id_col = 'referral.unique_service_request_identifier' %}
-    {% endif %}
-
     SELECT
         ROW_NUMBER() OVER (
-            PARTITION BY {{ unique_id_col }}
+            PARTITION BY 'tbl.unique_service_request_identifier'
             {%- for col in partition_cols %}
                 , tbl.{{ col }}
             {%- endfor %}
@@ -27,14 +19,7 @@
 
     INNER JOIN {{ref('raw_csds_activesubmission')}} AS a
         ON tbl.unique_submission_id = a.unique_submission_id
-
-    {% if not is_referral %}
-
-        INNER JOIN {{ref('raw_csds_cyp101referral')}} AS referral
-            ON tbl.record_number = referral.record_number
-
-    {% endif %}
-    
+ 
     QUALIFY sequence = 1
 
 {% endmacro %}
