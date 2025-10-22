7965c7ecdbc9e3d62bf79d285ea35bb6c8a338ef
Jessica Roberts <jessica.roberts67@nhs.net>
2025-10-22 14:28:16 +0100

    Updating dedupe logic and adding in staging model for active submission (needed for cleaning)

--- models/staging/commissioning/stg_mhsds_spell.sql
+++ models/staging/commissioning/stg_mhsds_spell.sql
@@ -2,7 +2,10 @@
     config(materialized = 'view')
 }}
 
-select hosp_prov_spell_id
+select 
+    uniq_hosp_prov_spell_num
+    , uniq_submission_id
+    , person_id
     , start_date_hosp_prov_spell
     , source_adm_mh_hosp_prov_spell
     , meth_adm_mh_hosp_prov_spell
@@ -10,6 +13,11 @@ select hosp_prov_spell_id
     , estimated_disch_date_hosp_prov_spell
 from {{ ref('raw_mhsds_mhs501hospprovspell') }}
 qualify row_number() over (
-    partition by hosp_prov_spell_id
-    order by dmic_date_added desc, rownumber_id desc
+    partition by uniq_hosp_prov_spell_num
+    order by effective_from desc
 ) = 1
+
+-- NB: De-deuplicated layer MHSDS.docx shared by Shak recommends to deduplicate using
+-- PARTITION BY [UniqServReqID],[UniqHospProvSpellNum]
+-- however this leaves duplicate records on uniq_hosp_prov_spell_num which seem to be for the same event (same admission date, provider, patient)
+-- using partition on uniq_hosp_prov_spell_num only for now
