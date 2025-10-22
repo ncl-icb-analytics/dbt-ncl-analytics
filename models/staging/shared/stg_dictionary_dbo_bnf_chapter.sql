a7a8cf666236a79103a1d86eb171b611fed8b304
Jessica Roberts <jessica.roberts67@nhs.net>
2025-10-22 17:06:00 +0100

    Adding EPD PC staging models

---
+++ models/staging/shared/stg_dictionary_dbo_bnf_chapter.sql
@@ -0,0 +1,14 @@
+select
+    sk_bnf_chapter_id,
+    sk_bnf_chapter_parent_id,
+    chapter_code,
+    chapter_code_alt,
+    chapter_code_alt_pad,
+    chapter_name,
+    chapter_path,
+    chapter_path_depth,
+    is_official_bnf,
+    url,
+    date_created,
+    date_update
+from {{ ref('raw_dictionary_dbo_bnf') }}
