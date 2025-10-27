daa5c8e861b3b6b695dd04b470f4756ea6283907
Thomas Shallcross <thomas.shallcross@nhs.net>
2025-10-27 16:55:37 +0000

    fix: csds deduplication macro

--- macros/deduplicate_csds.sql
+++ macros/deduplicate_csds.sql
@@ -3,23 +3,34 @@
 {% macro deduplicate_csds(
         csds_table,
         partition_cols = []
-        ) %}
+    ) %}
+
+    {# 
+        This macro deduplicates a CSDS table by keeping the most recent record
+        (based on `effective_from`) within each unique combination of `partition_cols`.
+
+        Example usage:
+          {{ deduplicate_csds('my_schema.my_table', ['unique_service_request_identifier', 'unique_care_contact_identifier']) }}
+    #}
+
+    {% if partition_cols | length == 0 %}
+        {{ exceptions.raise_compiler_error("You must provide at least one partition column to deduplicate_csds.") }}
+    {% endif %}
 
     SELECT
         ROW_NUMBER() OVER (
-            PARTITION BY tbl.unique_service_request_identifier
+            PARTITION BY 
             {%- for col in partition_cols %}
-                , tbl.{{ col }}
+                {{ "tbl." ~ col }}{% if not loop.last %}, {% endif %}
             {%- endfor %}
             ORDER BY tbl.effective_from DESC
         ) AS sequence,
-        tbl.* 
+        tbl.*
+    FROM {{ csds_table }} AS tbl
 
-    FROM {{csds_table}} AS tbl
-
-    INNER JOIN {{ref('raw_csds_activesubmission')}} AS a
+    INNER JOIN {{ ref('raw_csds_activesubmission') }} AS a
         ON tbl.unique_submission_id = a.unique_submission_id
- 
+
     QUALIFY sequence = 1
 
 {% endmacro %}
