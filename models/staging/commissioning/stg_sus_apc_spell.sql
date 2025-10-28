657bd1af6103cfad40c7ece631a92c493668ff8c
Emily Baldwin <emily.baldwin20@nhs.net>
2025-10-28 20:21:54 +0000

    fix: merge conflict

--- models/staging/commissioning/stg_sus_apc_spell.sql
+++ models/staging/commissioning/stg_sus_apc_spell.sql
@@ -2,21 +2,6 @@
     config(materialized = 'view')
 }}
 
-<<<<<<< Updated upstream
-select primarykey_id
-    , spell_patient_identity_nhs_number_value_pseudo as sk_patient_id
-    , spell_care_location_site_code_of_treatment
-    , spell_admission_date
-    , spell_discharge_length_of_hospital_stay
-    , SPELL_COMMISSIONING_SERVICE_AGREEMENT_PROVIDER
-    , spell_commissioning_grouping_core_hrg
-    , spell_clinical_coding_grouper_derived_primary_diagnosis
-    , spell_clinical_coding_grouper_derived_secondary_diagnosis
-    , spell_clinical_coding_grouper_derived_dominant_procedure
-    , spell_admission_admission_sub_type
-    , spell_admission_admission_type
-    , spell_commissioning_tariff_calculation_final_price
-=======
 with valid_orgs as(
     select organisation_code 
     from {{ ref('stg_dictionary_dbo_organisation') }} 
@@ -52,5 +37,4 @@ from core_data as core
 
 left join valid_orgs vo 
     on core.SPELL_COMMISSIONING_SERVICE_AGREEMENT_PROVIDER = vo.organisation_code
->>>>>>> Stashed changes
 
