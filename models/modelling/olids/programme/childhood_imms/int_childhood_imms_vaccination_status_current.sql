bfbef8a885b266dad31b5fef37599d15bd6472d3
KatsplatH <kate.homer2@nhs.net>
2025-11-11 16:38:45 +0000

    feat: added new vaccine logic for MMRV

--- models/modelling/olids/programme/childhood_imms/int_childhood_imms_vaccination_status_current.sql
+++ models/modelling/olids/programme/childhood_imms/int_childhood_imms_vaccination_status_current.sql
@@ -28,16 +28,18 @@ ec.PERSON_ID
 ,ec.DOSE_NUMBER
 ,ec.ELIGIBLE_FROM_DATE 
 ,ec.ELIGIBLE_TO_DATE
-,ec.VACCINE_ELIGIBLE
+,ec.NEW_VACCINE_APPLICABLE
 ,ec.MAXIMUM_AGE_DAYS
 ,CASE 
 WHEN ve.EVENT_TYPE = 'Administration' AND OUT_OF_SCHEDULE = 'No' THEN 'Completed'  
 WHEN ve.EVENT_TYPE = 'Administration' AND OUT_OF_SCHEDULE = 'Yes' THEN 'OutofSchedule'
 WHEN ve.EVENT_TYPE = 'Declined' THEN 'Declined'  
 WHEN ve.EVENT_TYPE = 'Contraindicated' THEN 'Contraindicated' 
-WHEN ve.EVENT_DATE IS NULL  and DATE(ec.ELIGIBLE_FROM_DATE) < CURRENT_DATE THEN 'Overdue' 
-ELSE 'Not due yet'
-END as VACCINATION_STATUS
+WHEN ve.EVENT_DATE IS NULL and ec.VACCINE_ID in ('6IN1_4','MMRV_1B','MMRV_2B','MMRV_1','MMRV_2','MMRV_1C') AND ec.new_vaccine_applicable = 'Yes' AND DATE(ec.ELIGIBLE_FROM_DATE) < CURRENT_DATE THEN 'Overdue' 
+WHEN ve.EVENT_DATE IS NULL and ec.VACCINE_ID in ('6IN1_4','MMRV_1B','MMRV_2B','MMRV_1','MMRV_2','MMRV_1C') AND ec.new_vaccine_applicable = 'No' THEN 'Not applicable'
+WHEN ve.EVENT_DATE IS NULL and ec.VACCINE_ID not in ('6IN1_4','MMRV_1B','MMRV_2B','MMRV_1','MMRV_2','MMRV_1C') AND ec.AGE_DAYS_APPROX > ec.maximum_age_days THEN 'No longer eligible' 
+WHEN ve.EVENT_DATE IS NULL and ec.VACCINE_ID not in ('6IN1_4','MMRV_1B','MMRV_2B','MMRV_1','MMRV_2','MMRV_1C') AND DATE(ec.ELIGIBLE_FROM_DATE) < CURRENT_DATE AND ec.AGE_DAYS_APPROX < ec.maximum_age_days THEN 'Overdue' 
+ELSE 'Not due yet' END AS VACCINATION_STATUS
 ,ve.EVENT_DATE as VACCINATION_DATE
 ,ve.OUT_OF_SCHEDULE
 ,ve.AGE_AT_EVENT_OBS
