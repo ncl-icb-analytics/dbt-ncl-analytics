6e69fb558b8b12c6e24a0febfd227014870b8d44
Emily Baldwin <emily.baldwin20@nhs.net>
2025-12-01 17:05:35 +0000

    fix: IP running off staging not int encounters. Name changed to IP over APC following stakeholder check-in

---
+++ models/reporting/commissioning/person_level/fct_person_sus_ip_recent.sql
@@ -0,0 +1,30 @@
+with 
+base_encounters as (
+    select *
+    from {{ ref('int_sus_ip_encounters') }}
+    where start_date between dateadd(month, -12, current_date()) and current_date()
+),  
+apc_encounter_summary as(
+    select
+        sk_patient_id
+        , count(distinct case when start_date between dateadd(month, -3, current_date()) and current_date() 
+                then visit_occurrence_id end) as apc_3mo
+        , count(distinct case when start_date between dateadd(month, -1, current_date()) and current_date() 
+                then visit_occurrence_id end) as apc_1mo
+        , count(distinct visit_occurrence_id) as apc_12mo 
+        , sum(duration) as apc_los_12mo -- TO DO: add inferred los to open spells in int_table
+    from base_encounters
+    group by 
+        sk_patient_id
+)
+
+SELECT
+    sk_patient_id
+    , zeroifnull(apc_3mo) as apc_3mo
+    , zeroifnull(apc_1mo) as apc_1mo
+    , zeroifnull(apc_12mo) as apc_12mo
+    , zeroifnull(apc_los_12mo) as apc_los_12mo
+from 
+    apc_encounter_summary as a
+
+where sk_patient_id is not null
