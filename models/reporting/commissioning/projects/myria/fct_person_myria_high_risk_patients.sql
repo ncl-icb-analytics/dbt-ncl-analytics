b565a25dd10eb135152555cea127ea551d7b22b8
PeterSNHS <peter.shakeshaft1@nhs.net>
2025-12-10 17:02:07 +0000

    feat: myria fct table (output)

---
+++ models/reporting/commissioning/projects/myria/fct_person_myria_high_risk_patients.sql
@@ -0,0 +1,60 @@
+{{ config(materialized="table") }}
+
+select 
+    patient_id,
+    local_authority,
+    barnet_hospital_count,
+    barnet_hospital_flag,
+    RFL_ex_BH_count,
+    RFL_ex_BH_flag,
+    RFL_count,
+    RFL_flag,
+    NCLProvider_count,
+    NCLProvider_flag,
+    Non_NCLProvider_count,
+    Non_NCLProvider_flag,
+    nel_ip_admissions_last_24_months,
+    nel_ip_admissions_last_12_months,
+    nel_ip_admissions_last_6_months,
+    heart_failure,
+    copd,
+    dementia,
+    end_stage_renal_failure,
+    severe_interstitial_lung_disease,
+    parkinsons_disease,
+    chronic_kidney_disease,
+    liver_failure,
+    alcohol_dependence,
+    bronchiectasis,
+    atrial_fibrillation,
+    cerebrovascular_disease,
+    peripheral_vascular_disease,
+    pulmonary_heart_disease,
+    coronary_heart_disease,
+    osteoporosis,
+    rheumatoid_arthritis,
+    chronic_liver_disease,
+    --- The following are not high risk 'conditions' but helpful flags for risk and multimorbidity
+    hypertension,
+    frailty_falls,
+from 
+    {{ ref("int_myria_conditions") }} 
+where
+    (heart_failure = 1 
+    or copd = 1 
+    or dementia = 1 
+    or end_stage_renal_failure = 1 
+    or severe_interstitial_lung_disease = 1 
+    or parkinsons_disease = 1 
+    or chronic_kidney_disease = 1 
+    or liver_failure = 1 
+    or alcohol_dependence = 1 
+    or bronchiectasis = 1 
+    or atrial_fibrillation = 1 
+    or cerebrovascular_disease = 1 
+    or peripheral_vascular_disease = 1 
+    or pulmonary_heart_disease = 1 
+    or coronary_heart_disease = 1 
+    or osteoporosis = 1 
+    or rheumatoid_arthritis = 1
+    or chronic_liver_disease = 1)
