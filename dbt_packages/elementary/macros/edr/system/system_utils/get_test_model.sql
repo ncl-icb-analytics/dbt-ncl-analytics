868362153e65cd1d0fddaea28db071351217eba3
Eddie Davison <143042680+EddieDavison92@users.noreply.github.com>
2025-12-16 22:52:43 +0000

    chore: remove profiles.yml and dbt_packages from version control (#286)
    Remove profiles.yml and dbt_packages from git tracking now that Snowflake workspaces provide native execution support. These files are local-only for developer use and should not be committed.
    
    Changes:
    - Add profiles.yml and dbt_packages/ to .gitignore
    - Remove tracked profiles.yml and dbt_packages from repository
    - Create profiles.yml.example as template for local development
    - Update start_dbt.ps1 to check for profiles.yml instead of using skip-worktree
    - Simplify pre-commit hooks to focus on preventing credential file commits
    
    Developers must run 'dbt deps' after pulling to reinstall packages locally.
    For local development, copy profiles.yml.example to profiles.yml.

--- dbt_packages/elementary/macros/edr/system/system_utils/get_test_model.sql
+++
@@ -1,6 +0,0 @@
-{% macro get_test_model() %}
-  {# This macro is used to get the global "model" object for tests,
-     we created it since context["model"] doesn't return the same thing in dbt-fusion #}
-
-  {% do return(model) %}
-{% endmacro %}
--- dbt_packages/elementary/macros/edr/system/system_utils/is_dbt_fusion.sql
+++
@@ -1,7 +0,0 @@
-{% macro is_dbt_fusion() %}
-    {% if dbt_version.split(".")[0] | int == 2 %}
-        {% do return(true) %}
-    {% endif %}
-
-    {% do return(false) %}
-{% endmacro %}
