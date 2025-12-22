71ceff982e36f5509fea740537a18d27fd1afad8
Jake Kealey <jake.kealey@nhs.net>
2025-12-22 14:47:04 +0000

    fix: Incorrect source column name replaced with correct one

--- models/staging/shared/stg_pds_pds_address.sql
+++ models/staging/shared/stg_pds_pds_address.sql
@@ -3,7 +3,7 @@ select
     pseudo_nhs_number as sk_patient_id, 
     der_postcode_sector as postcode_sector, 
     der_currentyr2021_lsoa_of_residence_from_postcode as lsoa_21, 
-    person_business_effective_from_date as event_from_date, 
-    person_business_effective_to_date as event_to_date
+    usual_address_business_effective_from_date as event_from_date, 
+    usual_address_business_effective_to_date as event_to_date
 
 from {{ref('raw_pds_pds_address')}}
