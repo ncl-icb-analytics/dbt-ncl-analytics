a56aa503e623e5f7c16f66a9086bff3dafd04b01
Jake Kealey <jake.kealey@nhs.net>
2025-12-22 13:32:08 +0000

    feat: Renamed column names in existing PDS staging tables to align the name of the event date columns. Renamed other fields in the Person table to indicate they are code fields.

--- models/staging/shared/stg_pds_pds_person.sql
+++ models/staging/shared/stg_pds_pds_person.sql
@@ -1,12 +1,13 @@
-select
-    row_id,
-    pseudo_nhs_number as sk_patient_id,
-    year_month_of_birth,
-    gender,
-    date_of_death,
-    death_status,
-    preferred_language,
-    interpreter_required,
-    person_business_effective_from_date,
-    person_business_effective_to_date
-from {{ ref('raw_pds_pds_person') }}
+select 
+    row_id, 
+    pseudo_nhs_number as sk_patient_id, 
+    to_date(year_month_of_birth, 'YYYYMM') as year_month_of_birth, 
+    gender as gender_code, 
+    date_of_death, 
+    death_status, 
+    preferred_language as preferred_language_code, 
+    interpreter_required as interpreter_required_code, 
+    person_business_effective_from_date as event_from_date, 
+    person_business_effective_to_date as event_to_date
+
+from {{ref('raw_pds_pds_person')}}
--- models/staging/shared/stg_pds_pds_person_merger.sql
+++ models/staging/shared/stg_pds_pds_person_merger.sql
@@ -2,6 +2,6 @@ select
     row_id,
     pseudo_nhs_number as sk_patient_id,
     pseudo_superseded_nhs_number as sk_patient_id_superseded,
-    person_merger_business_effective_from_date,
-    person_merger_business_effective_to_date
+    person_merger_business_effective_from_date as event_from_date,
+    person_merger_business_effective_to_date as event_to_date
 from {{ ref('raw_pds_pds_person_merger') }}
