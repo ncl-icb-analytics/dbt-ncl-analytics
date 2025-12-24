1fc4ddbfb45d375285984fe4e9dfbe71e108e0df
Jake Kealey <jake.kealey@nhs.net>
2025-12-24 14:14:56 +0000

    feat: Type casted staging table date fields to dates to prevent datetime columns

--- models/staging/shared/stg_pds_pds_address.sql
+++ models/staging/shared/stg_pds_pds_address.sql
@@ -19,8 +19,8 @@ select
     pseudo_nhs_number as sk_patient_id, 
     der_postcode_sector as postcode_sector, 
     der_currentyr2021_lsoa_of_residence_from_postcode as lsoa_21, 
-    usual_address_business_effective_from_date as event_from_date, 
-    usual_address_business_effective_to_date as event_to_date
+    to_date(usual_address_business_effective_from_date) as event_from_date, 
+    to_date(usual_address_business_effective_to_date) as event_to_date
 
 from {{ref('raw_pds_pds_address')}}
 
--- models/staging/shared/stg_pds_pds_person.sql
+++ models/staging/shared/stg_pds_pds_person.sql
@@ -3,11 +3,11 @@ select
     pseudo_nhs_number as sk_patient_id, 
     to_date(year_month_of_birth, 'YYYYMM') as year_month_of_birth, 
     gender as gender_code, 
-    date_of_death, 
+    to_date(date_of_death) as date_of_death, 
     death_status, 
     preferred_language as preferred_language_code, 
     interpreter_required as interpreter_required_code, 
-    person_business_effective_from_date as event_from_date, 
-    person_business_effective_to_date as event_to_date
+    to_date(person_business_effective_from_date) as event_from_date, 
+    to_date(person_business_effective_to_date) as event_to_date
 
 from {{ref('raw_pds_pds_person')}}
