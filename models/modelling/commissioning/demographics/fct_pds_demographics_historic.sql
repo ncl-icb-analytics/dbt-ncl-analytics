cd69dae4c78ec6323cd79685d972f6313e7260f5
Jake Kealey <jake.kealey@nhs.net>
2026-01-07 14:55:07 +0000

    add: Snapshot version of the pds demographics table

---
+++ models/modelling/commissioning/demographics/fct_pds_demographics_historic.sql
@@ -0,0 +1,100 @@
+select
+        --Record information
+        pds.snapshot_year,
+        pds.snapshot_date,
+        pds.sk_patient_id,
+        pds.record_registered_end_date is null as current_ncl_person_flag,
+        pds.record_person_end_date,
+
+        --Person information
+        pds.year_month_of_birth,
+        dict_g.gender,
+        pds.date_of_death,
+        dict_pl.preferred_language,
+        dict_ir.interpreter_required,
+    
+        --Registered information
+        (
+                --NCL Practice
+                dict_pcn.stp_code = 'QMJ' and
+                --The GP Practice is open (at time of snapshot)
+                coalesce(dict_gp.end_date, '9999-12-31') >= snapshot_date and
+                --No record of death
+                pds.date_of_death is null and
+                --No record of the record being removed
+                pds.registered_reason_for_removal is null
+                
+        ) as ncl_registered_flag,
+        pds.record_registered_start_date,
+        pds.record_registered_end_date,
+        pds.practice_code,
+        dict_gp.organisation_name as practice_name,
+        dict_pcn.network_code as pcn_code,
+        dict_pcn.network_name as pcn_name,
+        dict_pcn.stp_code as icb_code,
+        dict_pcn.stp_name as icb_name,
+        geo_reg.local_authority_2025_name as registered_borough,
+        nb_reg.neighbourhood_code as registered_neighbourhood_code,
+        nb_reg.neighbourhood_name as registered_neighbourhood_name,
+
+        --Resident information
+        (
+                --NCL Address
+                geo.resident_flag = 'NCL' and
+                --No record of death
+                pds.date_of_death is null
+        ) as ncl_resident_flag,
+        pds.record_residence_end_date,
+        pds.postcode_sector,
+        pds.lsoa_21,
+        geo.ward_2025_code,
+        geo.ward_2025_name,
+        geo.local_authority_2025_name as residence_borough,
+        nb_res.neighbourhood_code as residence_neighbourhood_code,
+        nb_res.neighbourhood_name as residence_neighbourhood_name,
+        imd.index_of_multiple_deprivation_decile as residence_imd_decile,
+        imd.index_of_multiple_deprivation_score as residence_imd_score,
+
+        --Ethnicity information
+        eth.* EXCLUDE (sk_patientid, record_date),
+
+        --Metadata information
+        cast(current_timestamp as datetime) as _timestamp
+        
+from {{ref('int_pds_snapshot_records')}} pds
+
+left join {{ref('stg_dictionary_dbo_gender')}} as dict_g
+on pds.gender_code = dict_g.gender_code
+
+left join dev__modelling.lookup_ncl.preferred_language as dict_pl
+on pds.preferred_language_code = dict_pl.code
+
+left join dev__modelling.lookup_ncl.interpreter_required as dict_ir
+on pds.interpreter_required = dict_ir.interpreter_required
+
+left join modelling.lookup_ncl.lsoa_2021_ward_2025_local_authority_2025 geo
+on pds.lsoa_21 = geo.lsoa_2021_code
+
+left join {{ref('raw_reference_lookup_ncl_ncl_neighbourhood_lsoa_2021_latest')}} nb_res
+on pds.lsoa_21 = nb_res.lsoa_2021_code
+
+left join dev__modelling.lookup_ncl.imd25_imd imd
+on pds.lsoa_21 = imd.lsoa_code_2021
+
+left join {{ref('stg_dictionary_dbo_organisation')}} dict_gp
+on pds.practice_code = dict_gp.organisation_code
+
+left join {{ref('stg_dictionary_dbo_organisationmatrixpracticeview')}} as dict_pcn
+on dict_gp.sk_organisation_id = dict_pcn.sk_organisation_id_practice
+
+left join "Dictionary"."dbo"."Postcode" gp_pc
+on dict_gp.sk_postcode_id = gp_pc."SK_Postcode_ID"
+
+left join modelling.lookup_ncl.lsoa_2021_ward_2025_local_authority_2025 geo_reg
+on gp_pc.lsoa = geo_reg.lsoa_2021_code
+
+left join {{ref('raw_reference_lookup_ncl_ncl_neighbourhood_lsoa_2021_latest')}} nb_reg
+on gp_pc.lsoa = nb_reg.lsoa_2021_code
+
+left join dev__modelling.lookup_ncl.ethnicity_national_data_sets eth
+ON pds.sk_patient_id = eth.sk_patientid
---
+++ models/modelling/commissioning/demographics/int_pds_snapshot_records.sql
@@ -0,0 +1,113 @@
+with snapshot_dates as (
+    select 
+        cast("FullDate" as date) as snapshot_date,
+        concat(year(snapshot_date) - 1, '/', year(snapshot_date) - 2000) as snapshot_year
+    --Hardcoded as table not in staging
+    from "Dictionary"."dbo"."Dates" ddd
+    --Bound range of dates
+    where year("FullDate") >= 2020
+    and current_date() > "FullDate"
+    --Last day of Financial Year
+    and month("FullDate") = 3
+    and day("FullDate") = 31
+),
+pds_person as (
+    select
+        sd.snapshot_year,
+        sd.snapshot_date,
+        sk_patient_id,
+        event_from_date as record_person_start_date,
+        event_to_date as record_person_end_date,
+        year_month_of_birth,
+        gender_code,
+        date_of_death,
+        preferred_language_code,
+        interpreter_required
+    from {{ref('stg_pds_pds_person')}} pds
+
+    --Join to get annual snapshots of the data
+    inner join snapshot_dates sd
+    on sd.snapshot_date between
+        pds.event_from_date and
+        coalesce(pds.event_to_date, '9999-12-31')
+
+    --Qualify to handle events that transition on the snapshot date without a day in-between
+    qualify row_number() over(
+        partition by sd.snapshot_date, pds.sk_patient_id
+        order by coalesce(pds.event_to_date, '9999-12-31') desc, pds.row_id desc
+    ) = 1
+),
+pds_registered as (
+    select
+        sd.snapshot_year,
+        sd.snapshot_date,
+        pds.sk_patient_id,
+        pds.event_from_date as record_registered_start_date,
+        pds.event_to_date as record_registered_end_date,
+        practice_code,
+        rfr.reason_for_removal as registered_reason_for_removal
+        
+    from {{ref('stg_pds_pds_patient_care_practice')}} pds
+
+        
+    --Join to get annual snapshots of the data
+    inner join snapshot_dates sd
+    on sd.snapshot_date between
+        pds.event_from_date and
+        coalesce(pds.event_to_date, '9999-12-31')
+
+    --Join to get reason for removal
+    left join dev__modelling.dbt_staging.stg_pds_pds_reason_for_removal rfr
+    on rfr.sk_patient_id = pds.sk_patient_id
+    --Snapshot date
+    and sd.snapshot_date between
+        rfr.event_from_date
+        and coalesce(rfr.event_to_date,'9999-12-31')
+    --Reason for removal must start after the record exists
+    and pds.event_from_date <= rfr.event_from_date
+
+    --Qualify to handle events that transition on the snapshot date without a day in-between
+    qualify row_number() over(
+        partition by sd.snapshot_date, pds.sk_patient_id
+        order by coalesce(pds.event_to_date, '9999-12-31') desc, pds.row_id desc
+    ) = 1
+),
+pds_address as (
+    select
+        sd.snapshot_year,
+        sd.snapshot_date,
+        sk_patient_id,
+        event_from_date as record_residence_start_date,
+        event_to_date as record_residence_end_date,
+        postcode_sector,
+        lsoa_21
+        
+    from {{ref('stg_pds_pds_address')}} pds
+
+    --Join to get annual snapshots of the data
+    inner join snapshot_dates sd
+    on sd.snapshot_date between
+        pds.event_from_date and
+        coalesce(pds.event_to_date, '9999-12-31')
+
+    --Qualify to handle events that transition on the snapshot date without a day in-between
+    qualify row_number() over(
+        partition by sd.snapshot_date, pds.sk_patient_id
+        order by coalesce(pds.event_to_date, '9999-12-31') desc, pds.row_id desc
+    ) = 1
+)
+
+--Script to combine the 3 PDS data tables into a single wider table
+select 
+    pds_person.*,
+    pds_registered.* exclude (snapshot_year, snapshot_date, sk_patient_id), 
+    pds_address.* exclude (snapshot_year, snapshot_date, sk_patient_id)
+from pds_person
+
+left join pds_registered on
+pds_person.sk_patient_id = pds_registered.sk_patient_id
+and pds_person.snapshot_date = pds_registered.snapshot_date
+
+left join pds_address on
+pds_person.sk_patient_id = pds_address.sk_patient_id
+and pds_person.snapshot_date = pds_address.snapshot_date
