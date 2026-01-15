fd86020b2a8d70ff6781c8673eaf07d0c1bee745
KatsplatH <kate.homer2@nhs.net>
2026-01-15 17:58:13 +0000

    feat: added fields to MHSDS

--- models/staging/commissioning/stg_mhsds_carecontact.sql
+++ models/staging/commissioning/stg_mhsds_carecontact.sql
@@ -7,7 +7,10 @@ select
     , uniq_submission_id
     , person_id
     , care_cont_date
+    , org_id_prov
+    , attend_or_dna_code
     , clin_cont_dur_of_care_cont
+    , dm_icb_commissioner
 from {{ ref('raw_mhsds_mhs201carecontact') }}
 qualify row_number() over (
     partition by uniq_care_cont_id
