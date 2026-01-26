4ad6421f5b0b67340bc309d3ed18283c868432c0
Jake Kealey <jake.kealey@nhs.net>
2026-01-26 10:58:03 +0000

    fix: Adjustments to correct errorous ethnicity code values and truncate values to 1 digit to align to other rows

--- models/modelling/commissioning/demographics/int_person_pmi_dataset_ethnicity_national_data_sets.sql
+++ models/modelling/commissioning/demographics/int_person_pmi_dataset_ethnicity_national_data_sets.sql
@@ -2,7 +2,14 @@ select
     'eth' as dataset_source,
     sk_patientid as sk_patient_id,
     --Trim to fix records with trailing spaces
-    trim(ethnicity_code) as ethnicity_code,
+    left(
+        case 
+            when trim(ethnicity_code) = '' then null
+            when right(ethnicity_code, 1) = '*' then left(ethnicity_code, length(ethnicity_code) - 1)
+            else trim(ethnicity_code) 
+        end, 
+        1
+    ) as ethnicity_code,
     cast(record_date as date) as ethnicity_event_date
     
 from {{ ref('stg_reference_lookup_ncl_ethnicity_national_data_sets') }}
