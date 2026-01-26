1e616518dbf23cfc95e51059f0eb11a9b6fb1fae
Jake Kealey <jake.kealey@nhs.net>
2026-01-26 09:11:52 +0000

    feat: Renamed output columns to remove dataset prefixes so they align and can be unioned rather than joined in the combined file. Also renamed lsoa

--- models/modelling/commissioning/demographics/int_person_pmi_dataset_ethnicity_national_data_sets.sql
+++ models/modelling/commissioning/demographics/int_person_pmi_dataset_ethnicity_national_data_sets.sql
@@ -1,7 +1,7 @@
 select
-    sk_patientid as eth_sk_patient_id,
+    sk_patientid as sk_patient_id,
     --Trim to fix records with trailing spaces
-    trim(ethnicity_code) as eth_ethnicity_code,
-    cast(record_date as date) as eth_ethnicity_event_date
+    trim(ethnicity_code) as ethnicity_code,
+    cast(record_date as date) as ethnicity_event_date
     
 from {{ ref('stg_reference_lookup_ncl_ethnicity_national_data_sets') }}
