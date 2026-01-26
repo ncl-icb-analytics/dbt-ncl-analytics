d637750f42a85183d370e887b38cc692db572143
Jake Kealey <134609350+JakeKea@users.noreply.github.com>
2026-01-26 13:20:30 +0000

    Issue_390_gp_neighbourhood_lookup (#422)
    For issue #390
    
    ---------
    
    Co-authored-by: Eddie Davison <143042680+EddieDavison92@users.noreply.github.com>

--- models/reporting/olids/organisation/dim_practice_neighbourhood.sql
+++ models/reporting/olids/organisation/dim_practice_neighbourhood.sql
@@ -12,9 +12,9 @@ Note: Working with dummy data so geographic information may be limited/placehold
 */
 
 SELECT
-    practicecode AS practice_code,
-    practicename AS practice_name,
-    localauthority AS local_authority,
-    practiceneighbourhood AS neighbourhood_registered
-FROM {{ ref('stg_reference_practice_neighbourhood_lookup') }}
-WHERE practicecode IS NOT NULL
+    gp_practice_code AS practice_code,
+    practice_name AS practice_name,
+    borough AS local_authority,
+    neighbourhood_name AS neighbourhood_registered
+FROM {{ ref('stg_reference_lookup_ncl_gp_practice') }}
+WHERE gp_practice_code IS NOT NULL
