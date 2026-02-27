3cc2360a222710cfdc685d3f00925109abb9689f
Emily Baldwin <emily.baldwin20@nhs.net>
2026-02-27 15:09:22 +0000

    fix: data for shortlisted and accepted people

--- models/published/direct_care/C_LTCS/cltcs_events.sql
+++ models/published/direct_care/C_LTCS/cltcs_events.sql
@@ -17,6 +17,17 @@ with inclusion_list as (
     from {{ ref('inclusion_cohort')}}
     where eligible = 1
 ),
+shortlisted_list as (
+    select pp.person_id as olids_id,sl.patient_id,sl.pcn_code
+    from {{ ref('cltcs_status_log_most_recent')}} sl
+    left join {{ref('dim_person_pseudo')}} pp on pp.sk_patient_id = sl.patient_id
+    where action in ('Added to shortlist', 'Accept for MDT')
+),
+complete_list as (
+    select * from inclusion_list
+    union all
+    select * from shortlisted_list
+),
 sus_ae_events as(
     select 
         il.patient_id,
@@ -27,7 +38,7 @@ sus_ae_events as(
         sus_events.pod as event_detail,
         sus_events.visit_occurrence_id::varchar as event_id
     from {{ ref('int_sus_ae_encounters')}} sus_events 
-    inner join inclusion_list il on il.patient_id = sus_events.sk_patient_id
+    inner join complete_list il on il.patient_id = sus_events.sk_patient_id
     where sus_events.start_date between dateadd(year, {{ measurement_cutoff }}, current_date()) and current_date()
 ),
 sus_apc_events as(
@@ -40,7 +51,7 @@ sus_apc_events as(
         admission_method_name as event_detail,
         sus_events.visit_occurrence_id::varchar as event_id
     from {{ ref('int_sus_ip_encounters')}} sus_events 
-    inner join inclusion_list il on il.patient_id = sus_events.sk_patient_id
+    inner join complete_list il on il.patient_id = sus_events.sk_patient_id
     where sus_events.start_date between dateadd(year, {{ measurement_cutoff }}, current_date()) and current_date()
 ),
 sus_op_events as(
@@ -53,7 +64,7 @@ sus_op_events as(
         sus_events.pod as event_detail,
         sus_events.visit_occurrence_id::varchar as event_id
     from {{ ref('int_sus_op_encounters')}} sus_events 
-    inner join inclusion_list il on il.patient_id = sus_events.sk_patient_id
+    inner join complete_list il on il.patient_id = sus_events.sk_patient_id
     where sus_events.start_date between dateadd(year, {{ measurement_cutoff }}, current_date()) and current_date()
 ),
 gp_events as (
@@ -66,7 +77,7 @@ gp_events as (
         gpa.national_slot_category_name as event_detail,
         gpa.encounter_id::varchar as event_id
     from {{ ref('int_gp_encounters_appt') }} gpa
-    inner join inclusion_list il on il.olids_id = gpa.person_id
+    inner join complete_list il on il.olids_id = gpa.person_id
     where gpa.start_date between dateadd(year, {{ measurement_cutoff }}, current_date()) and current_date()
     and gpa.code not in ('3', '0')
 ), 
---
+++ models/published/direct_care/C_LTCS/cltcs_status_log_most_recent.sql
@@ -0,0 +1,8 @@
+select patient_id
+    , pcn_code
+    , mdt_date
+    , action
+    , action_date
+    , criteria
+from {{ ref('raw_c_ltcs_status_log') }}
+qualify row_number() over (partition by patient_id order by action_date desc) = 1
