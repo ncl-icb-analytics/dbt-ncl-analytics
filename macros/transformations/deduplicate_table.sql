d742522d02bd9ab47091ec4f62d40713d0f6be41
Thomas Shallcross <thomas.shallcross@nhs.net>
2026-03-09 14:04:22 +0000

    refactor: update deduplicate_table macro to support multiple order columns
      Rename order_col to order_cols and require a list argument. Add input
      validation mirroring partition_cols checks (must be non-empty list, not
      a string). Update all callers (int_eosinophil_count, int_eosinophil_percentage,
      int_feno_all) to use the new parameter with ['date_recorded', 'id'].

--- macros/transformations/deduplicate_table.sql
+++ macros/transformations/deduplicate_table.sql
@@ -3,23 +3,30 @@
 {% macro deduplicate_table(
         table,
         partition_cols,
-        order_col
+        order_cols
     ) %}
 
-    {# 
-        This macro deduplicates a table by 
+    {#
+        This macro deduplicates a table by
 
         dedup_table (dbt table ref):
             a ref to the raw csds table to be deduplicated
-         
+
         partition_cols (list[str]):
             a list of column(s) present in the dedup_table which the deduplication occurs over
+
+        order_cols (list[str]):
+            a list of column(s) to order by when picking the row to keep (DESC)
 
         Example usage:
     #}
 
     {% if partition_cols | length == 0 %}
         {{ exceptions.raise_compiler_error("You must provide at least one partition column to deduplicate_table.") }}
+    {% endif %}
+
+    {% if order_cols is not iterable or order_cols is string or order_cols | length == 0 %}
+        {{ exceptions.raise_compiler_error("You must provide order_cols as a non-empty list to deduplicate_table.") }}
     {% endif %}
 
     SELECT
@@ -31,7 +38,7 @@
             {{ "tbl." ~ col }}{% if not loop.last %}, {% endif %}
         {%- endfor %}
         ORDER BY
-        {%- for col in order_col %}
+        {%- for col in order_cols %}
             tbl.{{ col }} DESC{% if not loop.last %},{% endif %}
         {%- endfor %}
     ) = 1
