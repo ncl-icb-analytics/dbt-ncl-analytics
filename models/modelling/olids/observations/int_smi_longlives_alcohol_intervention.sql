edf4fc3efdcde71addb7aec2d443c3a233837809
Kate Homer <kate.homer2@nhs.net>
2026-03-23 18:01:03 +0000

    feat: added new model for lifestyle enhanced checks

--- models/modelling/olids/observations/int_smi_longlives_alcohol_intervention.sql
+++ models/modelling/olids/observations/int_smi_longlives_alcohol_intervention.sql
@@ -5,7 +5,7 @@
         tags=['smi_registry']
         )
 }}
---using mixed codes defined by SMI Longer Lives Campaign Alcohol Misuse education and referral to treatment. Added 2 extra codes from PCDrefset
+--using mixed codes defined by SMI Longer Lives Campaign Alcohol Misuse education and referral to treatment. Added 2 extra codes from PCDrefset. Also include declined treatment codes.
 WITH ALC as (
 SELECT
     obs.id,
@@ -13,7 +13,8 @@ SELECT
     obs.clinical_effective_date,
     obs.mapped_concept_code AS concept_code,
     obs.mapped_concept_display AS concept_display,
-FROM ({{ get_observations("'SMI_LONGER_LIVES_ALCOHOL_EDUCATION','ALCOHOLINT_COD'") }}) obs
+    obs.cluster_id
+FROM ({{ get_observations("'SMI_LONGER_LIVES_ALCOHOL_EDUCATION','ALCOHOLINT_COD','ALCSPADVDEC_COD'") }}) obs
 WHERE obs.clinical_effective_date IS NOT NULL 
 AND obs.clinical_effective_date <= CURRENT_DATE() -- No future dates
 )
@@ -22,5 +23,11 @@ select person_id
 ,clinical_effective_date
 ,concept_code
 ,concept_display
+,CASE 
+WHEN cluster_id = 'ALCOHOLINT_COD' THEN 'Yes'
+WHEN cluster_id = 'ALCSPADVDEC_COD' THEN 'Declined' 
+WHEN cluster_id = 'SMI_LONGER_LIVES_ALCOHOL_EDUCATION' AND concept_code <> '21121000175100' THEN 'Yes' 
+WHEN cluster_id = 'SMI_LONGER_LIVES_ALCOHOL_EDUCATION' AND concept_code = '21121000175100' THEN 'Declined'
+END AS alcohol_advice_services
 from ALC
 QUALIFY ROW_NUMBER() OVER (PARTITION BY PERSON_ID, CONCEPT_CODE, CLINICAL_EFFECTIVE_DATE ORDER BY PERSON_ID) = 1
---
+++ models/modelling/olids/programme/severe_mental_illness/int_smi_lifestyle_es.sql
@@ -0,0 +1,61 @@
+{{
+    config(
+        materialized='table',
+        cluster_by=['person_id'],
+        tags=['smi_registry']
+        )
+}}
+--Population demographics + Lifestyle enhanced metrics wide table
+select
+p.PERSON_ID
+,p.HX_FLAKE
+,p.PRACTICE_CODE
+,p.practice_name
+,p.RESIDENTIAL_LOC
+,p.RESIDENTIAL_BOROUGH
+-- ,p.RESIDENTIAL_NEIGHBOURHOOD
+-- ,p.WARD_CODE
+-- ,p.WARD_NAME
+,p.AGE
+,p.AGE_BAND_5Y
+,p.GENDER
+,p.ETHNICITY_CATEGORY
+,p.ETHCAT_ORDER
+,p.ETHNICITY_SUBCATEGORY
+,p.ETHSUBCAT_ORDER
+,p.IMD_QUINTILE
+,p.IMDQUINTILE_ORDER
+,p.MAIN_LANGUAGE
+,p.is_homeless
+--latest illicit drug use and subs misuse service referrals
+,NVL2(ILLICIT_DRUG_DATE, 'Yes', 'No') AS drug_use_assessed_ever
+,ILLICIT_DRUG_DATE as drug_assess_date
+,drug_misuse_flag
+,SM_INT_DATE
+,subs_misuse_services
+--smoking status ever and smoking cessation referrals
+,smok_status_last_12m
+,IFF(smok_status_DATE IS NULL, 'No', 'Yes') AS smoking_assessed_ever
+,smok_status_DATE 
+,SMOKER_FLAG
+,SMOK_INT_DATE
+,SMOKING_CESSATION_SERVICES
+--alcohol use ever and alcohol interventions
+,IFF(ALC_STAT_DATE IS NULL, 'No', 'Yes') AS alcohol_assessed_ever
+,ALC_STAT_DATE
+,high_alcohol_use_flag
+,alcohol_advice_services
+--nutrition assessment and diet
+,NVL2(NUTR_REV_DATE, 'Yes', 'No') AS diet_assessed_ever
+,NUTR_REV_DATE
+,POOR_DIET_FLAG
+,REFERRAL_DIET_ADVICE
+--exercise assessment and referral
+,NVL2(EX_STAT_DATE, 'Yes', 'No') AS exercise_assessed_ever
+,EX_STAT_DATE
+,LOW_EXERCISE_FLAG
+,REFERRAL_EXERCISE_ADVICE
+FROM {{ ref('int_smi_population_base')  }} p
+--FROM MODELLING.OLIDS_PROGRAMME.INT_SMI_POPULATION_BASE p
+LEFT JOIN {{ ref('int_smi_lifestyle')  }} l using (person_id)
+--LEFT JOIN DEV__MODELLING.OLIDS_PROGRAMME.INT_SMI_LIFESTYLE l using (person_id)
