e76d55541b570a48be12d0b9b89d77031ce2a9c6
Emily Baldwin <emily.baldwin20@nhs.net>
2026-05-01 16:58:06 +0100

    Feat: Updated cohorting logic to allow data to passthrough for people that drop out of inclusion list whilst being processed

--- models/published/direct_care/C_LTCS/cltcs_full_detailed_patient_list.sql
+++ models/published/direct_care/C_LTCS/cltcs_full_detailed_patient_list.sql
@@ -1,29 +1,34 @@
 with inclusion_list as (
-    select olids_id, patient_id, area_code, 'inclusion' as source
+    select patient_id, area_code, olids_id, 'inclusion' as source
     from {{ ref('cltcs_live_inclusion_cohort') }}
     where eligible = 1 and fragmented_sk_patient_id_flag = 0 and fragmented_person_id_flag = 0 -- exclude fragmented patients for now
 
 ),
 dropped_list as ( -- people who were shortlisted but dropped out of inclusion cohort
-    select arc.patient_id, arc.area_code, sl.olids_id, 'shortlist' as source
-    from {{ ref('cltcs_inclusion_cohort_archive') }} arc
-    inner join shortlisted_list sl on arc.patient_id = sl.patient_id
+    select arc.patient_id, arc.area_code, pp.person_id as olids_id, 'shortlist' as source
+    from {{ ref('stg_c_ltcs_inclusion_cohort_archive') }} arc
+    left join {{ref('dim_person_pseudo')}} pp on pp.sk_patient_id = arc.patient_id
     where cohort_event = 'DELETE' and is_active = TRUE
+    qualify row_number() over (partition by patient_id order by event_written_at desc) = 1
 ),
 excluded_list as ( -- no rereview or reconsideration within a year?
     select patient_id
     from {{ ref('cltcs_status_log_most_recent') }}
     where action in ('Reject', 'Case closed') 
-    and action_date is between dateadd(year, -1, current_date()) and current_date()
+    and action_date between dateadd(year, -1, current_date()) and current_date()
 ),
 complete_list as (
     select * from inclusion_list
     union all
     select * from dropped_list
 )
-select olids_id, patient_id, area_code
-from complete_list
-where patient_id is not in (select patient_id from excluded_list)
+
+select patient_id, area_code, olids_id, source
+from complete_list c
+where not exists (
+  select 1
+  from excluded_list e
+  where e.patient_id = c.patient_id)
 qualify row_number() over (
     partition by patient_id
     order by case when source = 'shortlist' then 0 else 1 end
