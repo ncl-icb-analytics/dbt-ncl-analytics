5a67d1cc851ec62e46f1755f31709e595ef8a7e2
Emily Baldwin <emily.baldwin20@nhs.net>
2026-05-18 10:21:08 +0100

    Fix: until snapshoots set up using just log for inclusion

--- models/published/direct_care/C_LTCS/cltcs_patient_list.sql
+++ models/published/direct_care/C_LTCS/cltcs_patient_list.sql
@@ -3,23 +3,29 @@ with registrant_list as (
     from {{ ref('cltcs_registrant_adult_population') }}
     where fragmented_sk_patient_id_flag = 0 and fragmented_person_id_flag = 0 -- exclude fragmented patients for now
 ),
-dropped_list as ( -- people who were shortlisted but dropped out of inclusion cohort
-    select arc.patient_id, arc.area_code, pp.person_id as olids_id, 'shortlist' as source
-    from {{ ref('stg_c_ltcs_inclusion_cohort_archive') }} arc
-    left join {{ref('dim_person_pseudo')}} pp on pp.sk_patient_id = arc.patient_id
-    where cohort_event = 'DELETE' and is_active = TRUE
-    qualify row_number() over (partition by patient_id order by event_written_at desc) = 1
+-- dropped_list as ( -- people who were shortlisted but dropped out of inclusion cohort
+--     select arc.patient_id, arc.area_code, pp.person_id as olids_id, 'shortlist' as source
+--     from {{ ref('stg_c_ltcs_inclusion_cohort_archive') }} arc
+--     left join {{ref('dim_person_pseudo')}} pp on pp.sk_patient_id = arc.patient_id
+--     where cohort_event = 'DELETE' and is_active = TRUE
+--     qualify row_number() over (partition by patient_id order by event_written_at desc) = 1
+-- ),
+shortlisted_list as (
+    select sl.patient_id, sl.area_code, pp.person_id as olids_id, 'shortlist' as source
+    from {{ ref('cltcs_status_log_most_recent') }} sl
+    left join {{ref('dim_person_pseudo')}} pp on pp.sk_patient_id = sl.patient_id
+    where action not in ('Reject', 'Case closed', 'Removed from shortlist') 
 ),
 excluded_list as ( -- no rereview or reconsideration within a year?
     select patient_id
-    from {{ ref('cltcs_status_log_most_recent') }}
+    from {{ ref('cltcs_status_log_most_recent') }} 
     where action in ('Reject', 'Case closed') 
     and action_date between dateadd(year, -1, current_date()) and current_date()
 ),
 complete_list as (
     select * from registrant_list
     union all
-    select * from dropped_list
+    select * from shortlisted_list
 ),
 
 expanded_registrant_list as (
