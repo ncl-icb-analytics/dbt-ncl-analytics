832308a34a62d54cf369c2184e0f32285e39f013
Jake Kealey <jake.kealey@nhs.net>
2026-06-05 09:52:36 +0100

    feat: Edited the names of the seed and models to use provider as the terminology.

--- models/staging/shared/stg_reference_cancer_provider_to_cancer_alliance.sql
+++ models/staging/shared/stg_reference_cancer_provider_to_cancer_alliance.sql
@@ -5,4 +5,4 @@ select
     ods_name                                as provider_name,
     cancer_alliance_name                    as cancer_alliance_name
 
-from {{ ref('trust_ods_to_cancer_alliance') }}
+from {{ ref('provider_ods_to_cancer_alliance') }}
