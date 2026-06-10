b5cdc6b7a33f67e62fb0275ffa47186bab2b9e5f
Emily Baldwin <emily.baldwin20@nhs.net>
2026-06-10 17:55:50 +0100

    Fix: diagnosis codes entered for amny episodes

--- models/modelling/commissioning/diagnosis/int_sus_ip_diagnosis.sql
+++ models/modelling/commissioning/diagnosis/int_sus_ip_diagnosis.sql
@@ -6,36 +6,32 @@
 -- `<CHAR><NUM><NUM>` or `<CHAR><NUM><NUM>.<NUM>`
 with
     final_icd_codes as (
-        select  diagnosis_id
-            ,primarykey_id
-            ,icd_id 
-            ,rownumber_id 
-            ,episodes_id
-            , {{ clean_icd10_code("CODE") }} as concept_code 
-            ,code
+        select primarykey_id
+            , code 
+            , count(*) as diagnosis_count
+            , array_agg(distinct episodes_id) as episodes_ids
         from {{ ref("stg_sus_apc_spell_episodes_clinical_coding_diagnosis_icd") }}
+        group by primarykey_id, code
 )
 
 select 
-    f.diagnosis_id,
     se.sk_patient_id,
     se.start_date as date,
     f.primarykey_id as visit_occurrence_id,
     'APC_SPELL' as visit_occurrence_type,
-    f.episodes_id,
-    icd_id,
-    f.rownumber_id,
     se.organisation_id,
     se.organisation_name,  -- join to reference
     se.site_id,
     se.site_name,  -- join to reference
-    f.concept_code as source_concept_code,
+    f.code as source_concept_code,
+    f.diagnosis_count,
+    f.episodes_ids,
     c.concept_code,
     c.concept_name,  -- mapped concept name from the vocabulary
     'ICD10' as concept_vocabulary
 from final_icd_codes f
 left join  {{ ref('stg_aic_base_athena_concept') }} c
-    on c.concept_code = f.concept_code
+    on c.concept_code = f.code
     and c.vocabulary_id = 'ICD10'
 left join {{ ref("int_sus_apc_encounter") }} se on se.visit_occurrence_id = f.primarykey_id
 
