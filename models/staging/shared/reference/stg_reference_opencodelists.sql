a4de1272030e87a8081a01471bb17a80416d2a98
Emily Baldwin <emily.baldwin20@nhs.net>
2026-06-24 10:22:18 +0100

    Fix: cannot dedup without arbitrarily selecting a term so implementing for the time being to preven fanning joins on codelists. Warning added to highlight this is arbritrary as user may prefer to add canonical terms  from lookups. Left this way incase there are old codes that we don't have terms for

--- models/staging/shared/reference/stg_reference_opencodelists.sql
+++ models/staging/shared/reference/stg_reference_opencodelists.sql
@@ -9,15 +9,17 @@ select
     cluster_id
 from {{ ref('raw_reference_opencodelists') }}
 where code is not null
-qualify row_number() over (
-    partition by
-        codelist_org,
-        codelist_slug,
-        codelist_name,
-        version_hash,
-        coding_system,
-        code,
-        term,
-        cluster_id
-    order by code
-) = 1
+qualify
+    row_number() over (
+        partition by
+            codelist_org,
+            codelist_slug,
+            codelist_name,
+            version_hash,
+            coding_system,
+            code,
+            cluster_id
+        order by
+            case when term is null then 1 else 0 end,
+            term
+    ) = 1
