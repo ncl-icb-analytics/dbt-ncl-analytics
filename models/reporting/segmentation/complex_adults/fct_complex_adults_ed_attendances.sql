77126749dc7c24e4a331059df108ca361c458007
EddieDavison92 <143042680+EddieDavison92@users.noreply.github.com>
2026-07-16 18:28:04 +0100

    refactor(segmentation): address review - int-layer times, data_tests syntax

--- models/reporting/segmentation/complex_adults/fct_complex_adults_ed_attendances.sql
+++ models/reporting/segmentation/complex_adults/fct_complex_adults_ed_attendances.sql
@@ -15,9 +15,9 @@ SELECT
     e.pod,
     e.department_type,
     e.start_date AS arrival_date,
-    ec.attendance_arrival_time AS arrival_time,
+    e.start_time AS arrival_time,
     e.end_date AS departure_date,
-    ec.attendance_departure_time AS departure_time,
+    e.end_time AS departure_time,
     e.organisation_id AS provider_code,
     e.organisation_name AS provider_name,
     e.site_id,
@@ -29,6 +29,4 @@ SELECT
 FROM {{ ref('int_sus_uec_encounter') }} AS e
 INNER JOIN {{ ref('fct_person_complex_adults') }} AS c
     ON e.sk_patient_id = c.sk_patient_id
-LEFT JOIN {{ ref('stg_sus_ae_emergency_care') }} AS ec
-    ON e.visit_occurrence_id = ec.primarykey_id
 WHERE e.start_date >= DATEADD(MONTH, -12, CURRENT_DATE())
