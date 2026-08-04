bba661bcbb698826d5815ade3fadf8eab765fdf0
Poya Khalaj <poya.khalaj@nhs.net>
2026-08-04 12:39:48 +0100

    Tested and ran the SUS AE Monthly EncounterDenormalised.

--- models/staging/commissioning/sus/stg_sus_commissioner_provider_postcode.sql
+++ models/staging/commissioning/sus/stg_sus_commissioner_provider_postcode.sql
@@ -3,5 +3,5 @@ select
     trim(commissioner_code) as commissioner_code,
     cast(effective_from as date) as effective_from,
     cast(effective_to as date) as effective_to
-from {{ ref('raw_sus_commissioner_provider_postcode') }}
+from {{ ref('raw_sus_commissioner_provider_postcode_commissioner') }}
 where cast(effective_from as date) <= cast(effective_to as date)
