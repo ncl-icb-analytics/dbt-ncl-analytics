364896c3b3f1fd3deffef86ca60eb39056265b00
Kate Homer <kate.homer2@nhs.net>
2026-08-17 16:00:18 +0100

    feat: practice DQ fix

--- models/published/direct_care/olids/adult_imms/practice_dq_fail_live_dm.sql
+++ models/published/direct_care/olids/adult_imms/practice_dq_fail_live_dm.sql
@@ -4,12 +4,12 @@
         tags=['adult_imms']
     )
 }}
---practice list with failing EMIS registration data quality checks. 
-select  p.BOROUGH_REGISTERED as borough 
+--practice list with failing EMIS registration data quality checks. medicus select care not reported in EMIS
+select  eq.borough 
 ,eq.practice_code 
-,p.practice_name 
-,eq.issue_category    
+,eq.practice_name 
+,eq.issue_category     
 from {{ ref('emis_olids_reg_fail_direct_care') }} eq   
 LEFT join (select distinct BOROUGH_REGISTERED, practice_code, practice_name 
 from {{ ref('dim_person_demographics') }}) p using (practice_code)   
-where practice_code <> 'Y03103' order by 1, 2
+where practice_code <> 'Y03103' order by 1, 2 
