eb0a1462bd5f6ce69d475b28c04ade228ed27164
EddieDavison92 <143042680+EddieDavison92@users.noreply.github.com>
2026-08-26 19:57:46 +0100

    revert: remove misplaced primary care changes

--- models/staging/shared/reference/stg_reference_primary_care_pcn_membership_all.sql
+++ models/staging/shared/reference/stg_reference_primary_care_pcn_membership_all.sql
@@ -8,6 +8,5 @@ select
     , practice_status
     , pcn_code
     , pcn_name
-    , health_borough_name
     , registered_borough_name
 from {{ ref('raw_reference_primary_care_pcn_membership_all') }}
