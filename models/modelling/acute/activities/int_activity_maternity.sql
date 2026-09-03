cf961d44001ec47c779581846d4ff129572ac6b5
Emily Baldwin <emily.baldwin20@nhs.net>
2026-09-03 17:16:02 +0100

    chore: disabling models that aren't actively being used and removing model that will be supersceded by improved cross-system view table

--- models/modelling/acute/activities/int_activity_maternity.sql
+++ models/modelling/acute/activities/int_activity_maternity.sql
@@ -1,3 +1,8 @@
+{{
+    config(
+        materialized='view',
+        enabled = false)
+}}
 with specialty_filters as (
     -- Combine filter conditions to avoid repetition
     select distinct visit_occurrence_id
