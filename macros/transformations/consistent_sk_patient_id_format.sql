d8eab0337f2c2a76800128c0d1476cf8158a0290
Emily Baldwin <emily.baldwin20@nhs.net>
2026-09-04 15:20:29 +0100

    Fix: 1 used as a placeholder for null so replacing with null to reduce downstream filtering need. Discussion suggests this in not an informative indicator e.g., chosen not to disclose vs missing.

--- macros/transformations/consistent_sk_patient_id_format.sql
+++ macros/transformations/consistent_sk_patient_id_format.sql
@@ -3,7 +3,8 @@
    be made in one place by updating this macro. Note, if considering switch to numeric then
    consider the colliding IDs with leading 0 e.g., 070 and 70 would colide. This was not observed
    when checked on 2026/09/04. A test has been added to flag if keys that aren't numeric begin to 
-   flow as the hxflake_pseudo_generation using a numeric transformation of this key #}
+   flow as the hxflake_pseudo_generation using a numeric transformation of this key. 1 used as a null
+   catch all so removing. Reintroduce if one has meaning #}
 {% macro consistent_sk_patient_id_format(column) -%}
     {%- if column is not string or column | trim == '' -%}
         {{ exceptions.raise_compiler_error(
@@ -12,5 +13,5 @@
             ~ "for a missing quote, e.g. (patient_id) instead of ('patient_id')."
         ) }}
     {%- endif -%}
-    nullif(trim(cast({{ column }} as varchar)), '')
+    nullif(nullif(trim(cast({{ column }} as varchar)), ''), '1')
 {%- endmacro %}
